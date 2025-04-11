886b4e166378d6ed2d529d7254e6703c013625a6
martinebucarey <martinebucarey@gmail.com>
2025-04-11 15:34:16 -0300

    Complete factory method

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -12,3 +12,75 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+interface Hamburger {
+  prepare(): void;
+}
+
+class ChickenHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing Chicken Hamburger");
+  }
+}
+
+class BeefHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing Beef Hamburger");
+  }
+}
+
+class BeanHamburger implements Hamburger {
+  prepare(): void {
+    console.log("Preparing Bean Hamburger");
+  }
+}
+
+abstract class Restaurant {
+  protected abstract createHamburger(): Hamburger;
+
+  orderHamburger(): void {
+    const hamburger = this.createHamburger();
+    hamburger.prepare();
+  }
+}
+
+class ChickenRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new ChickenHamburger();
+  }
+}
+
+class BeefRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new BeefHamburger();
+  }
+}
+
+class BeanRestaurant extends Restaurant {
+  override createHamburger(): Hamburger {
+    return new BeanHamburger();
+  }
+}
+
+function main() {
+  let restaurant: Restaurant;
+  const burgerType = prompt(
+    "que tipo de hamburguesa quieres?(chicken/beef/bean)"
+  );
+  switch (burgerType) {
+    case "chicken":
+      restaurant = new ChickenRestaurant();
+      break;
+    case "beef":
+      restaurant = new BeefRestaurant();
+      break;
+    case "bean":
+      restaurant = new BeanRestaurant();
+      break;
+    default:
+      throw new Error("Tipo de hamburguesa no válido");
+  }
+  restaurant.orderHamburger();
+}
+
+main();
