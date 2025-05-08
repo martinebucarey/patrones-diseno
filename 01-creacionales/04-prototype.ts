0dc40a9d55abcd296de16ea595c7fea6f6cd8d2a
martinebucarey <martinebucarey@gmail.com>
2025-05-08 17:05:15 -0300

    Add prototype

--- 01-creacionales/04-prototype.ts
+++ 01-creacionales/04-prototype.ts
@@ -9,3 +9,40 @@
  * 
  * https://refactoring.guru/es/design-patterns/prototype
  */
+
+class Documento {
+  public title: string;
+  private content: string;
+  public author: string;
+
+  constructor(title: string, content: string, author: string) {
+    this.title = title;
+    this.content = content;
+    this.author = author;
+  }
+
+  clone(): Documento {
+    return new Documento(this.title, this.content, this.author);
+  }
+
+  displayInfo() {
+    console.log(`
+        Title: ${this.title}
+        Content: ${this.content}
+        Author: ${this.author}`);
+  }
+}
+
+function main() {
+  const document1 = new Documento("Cotizacion", "500 dolares", "Fernando");
+
+  console.log({ document1 });
+  document1.displayInfo();
+
+  const document2 = document1.clone();
+  document2.title = "Nueva cotizacion";
+  console.log({ document2 });
+  document2.displayInfo();
+}
+
+main();
